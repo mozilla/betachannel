fd5759e877c1fceddd07dbbd66e15dcf8ff13f26
Austin King <shout@ozten.com>
2014-05-16 10:27:25 -0700

    Update help with latest CLI which does not use Python or NSS

--- server/lib/keygen.js
+++ server/lib/keygen.js
@@ -19,21 +19,36 @@ exports.createKeypair = function(binPath, configCertsDir, derFilePath, cb) {
 
   // TODO: do this ./generate_cert.sh $PWD $PWD/phone-cert.der
   // this will have the side effect of generatign the DER
-
   var generateCertCommand = [binPath + '/generate_cert.sh', configCertsDir, derFilePath].join(' ');
+
+  var derBasename = derFilePath.substring(0, derFilePath.length - ('.der'.length));
+  var publicDir = path.join(path.dirname(configCertsDir), 'public');
+  var generatePhoneCertDB = [binPath + '/generate_phone_cert_db.sh', derBasename, publicDir].join(' ');
 
   console.log(generateCertCommand);
 
-  exec(generateCertCommand, function(error, stdout, stderr) {
+  exec(generateCertCommand, function(err, stdout, stderr) {
     if (stdout) console.log('STDOUT', stdout);
-    if (error) {
-      console.log('ERROR', error);
+    if (err) {
+      console.log('ERROR', err);
       console.log('STDERR', stderr);
+      return cb(err);
     }
-    if (typeof cb === 'function') {
-      cb(error);
-    }
+
+    exec(generatePhoneCertDB,
+      function(err, stdout, stderr) {
+        console.log('STDOUT', stdout);
+        if (err) {
+          console.log('STDERR', stderr);
+        } else {
+          ['cert9.db', 'key4.db', 'pkcs11.txt'].forEach(function(pubFile) {
+            fs.symlinkSync(path.join(publicDir, 'certdb.tmp' , pubFile), path.join(path.resolve('www'), pubFile));
+          });
+        }
+        cb(err);
+    });
   });
+
 };
 
 exports.signAppPackage = function(binPath, configCertsDir, inputFile, outputFile, cb) {
