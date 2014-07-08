3af2b2197653254f46856369b5e2b772e3239ef9
Austin King <shout@ozten.com>
2014-07-08 14:04:41 -0700

    Detect when apps are already installed. Fixes Issue#47

--- www/js/app_install.js
+++ www/js/app_install.js
@@ -1,24 +1,47 @@
 (function() {
+
+  var installedApps = {};
+
   var install = document.querySelector('button[data-package-manifest-url]');
 
   if (install) {
-    if (!navigator.mozApps || !navigator.mozApps.installPackage) {
+    if (!navigator.mozApps || !navigator.mozApps.installPackage || !navigator.mozApps.getInstalled) {
       document.getElementById('needs-mozapps').classList.remove('hidden');
     } else {
+      install.classList.remove('hidden');
+      var request = window.navigator.mozApps.getInstalled();
+      request.onerror = function(e) {
+        alert("Error calling getInstalled: " + request.error.name);
+      };
+      request.onsuccess = function(e) {
+
+        for (var i = 0; i < request.result.length; i++) {
+          var manifestURL = request.result[i].manifestURL;
+          installedApps[manifestURL] = request.result[i];
+          $('[data-package-manifest-url="' + manifestURL + '"]')
+            .removeClass('install')
+            .addClass('launch')
+            .text('Launch');
+        }
+      };
 
       install.disabled = false;
 
       install.addEventListener('click', function f(e) {
         var url = install.getAttribute('data-package-manifest-url');
-        var request = navigator.mozApps.installPackage(url);
-        request.onsuccess = function() {
-          // if (!this.result || !this.result.manifest) {
-          // 	return alert('Install failed without error');
-          // }
-          // TODO replace "install" button with 'installed' widget
-        };
-        request.onerror = function() {
-          alert(this.error.name);
+        if ($(install).hasClass('install')) {
+          var request = navigator.mozApps.installPackage(url);
+          request.onsuccess = function() {
+            // if (!this.result || !this.result.manifest) {
+            // 	return alert('Install failed without error');
+            // }
+            // TODO replace "install" button with 'installed' widget
+          };
+          request.onerror = function() {
+            alert(this.error.name);
+          }
+        } else if ($(install).hasClass('launch')) {
+          installedApps[url].launch();
         }
       });
     }
